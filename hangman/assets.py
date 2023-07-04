74c596e2e283200aa64329a7f49ade22253ac836
Koigor97 <turaysamuel101@yahoo.com>
2023-07-04 12:08:41 +0200

    Add the hangman game file

---
+++ hangman/assets.py
@@ -0,0 +1,109 @@
+# List of words
+list_of_words = ['Abjure', 'Future', 'Picnic', 'Agnostic', 'Garland', 'Protect', 'Airline', 'Gigantic', 'Publish',
+                 'Bandit', 'Goofy', 'Quadrangle', 'Banquet', 'Government', 'Recount', 'Binoculars', 'Grandnieces',
+                 'Redoubtable', 'Biologist', 'Handbook', 'Reflection','Blackboard', 'Himself', 'Reporter', 'Board',
+                 'Ring', 'Indulge', 'Bookworm', 'Inflatable', 'Salesclerk', 'Butterscotch', 'Inimical', 'Snapshot',
+                 'Camera', 'Interim', 'Shellfish', 'Campus', 'Invest', 'Ship', 'Catfish', 'Jackpot', 'Significance',
+                 'Carsick', 'Kitchenette', 'Sometimes', 'Celebrate', 'Law', 'Sublime', 'Celery', 'Life', 'Tabletop',
+                 'Citizen', 'Lifeline', 'Teamwork', 'Coloring', 'Love', 'Tennis', 'Compact', 'Magnificent', 'Timesaving',
+                 'Dark', 'Malevolence', 'Tree', 'Damage', 'Man', 'Termination', 'Dangerous', 'Mascot', 'Underestimate',
+                 'Decorum', 'Marshmallow', 'Vineyard', 'Endorse', 'Mine', 'War', 'Engender', 'Moonwalk', 'Way', 'Near',
+                 'Erratic', 'Wealth', 'Envelope', 'Nephogram', 'Wednesday', 'Etymology', 'Newborn', 'World', 'Noisome',
+                 'Eyewitness', 'Xerox', 'Eulogy', 'Owl', 'You', 'Fish', 'Parenthesis', 'Zestful', 'Food', 'Perpetrator',
+                 'Foreclose', 'Phone']
+
+
+title = """
+██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗
+██║  ██║██╔══██╗████╗  ██║██╔════╝ ████╗ ████║██╔══██╗████╗  ██║
+███████║███████║██╔██╗ ██║██║  ███╗██╔████╔██║███████║██╔██╗ ██║
+██╔══██║██╔══██║██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║
+██║  ██║██║  ██║██║ ╚████║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+                                                                
+"""
+
+the_gallows = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hangman/main.py
@@ -0,0 +1,34 @@
+from assets import list_of_words, title, the_gallows
+import random
+
+banner = title
+hidden_word = random.choice(list_of_words).lower()
+word_in_char = []
+is_there_underscore = False
+chances_to_live = 6
+
+
+print(f"\n{banner}\nHey Hero. Time to use that hero brain 🧠 and save the day ✊🏾")
+
+for n in range(len(hidden_word)):
+    word_in_char.append("_")
+
+while not is_there_underscore:
+    print(f"\nThe secret word is: {''.join(word_in_char)} 🤷🏽‍\n{the_gallows[chances_to_live]}")
+    user_guess = input("Guess the letters of the secret word 🤭: ").lower()
+
+    for letter in range(len(hidden_word)):
+        if user_guess == hidden_word[letter]:
+            word_in_char[letter] = user_guess
+
+    if user_guess not in word_in_char:
+        chances_to_live -= 1
+        if chances_to_live == 0:
+            is_there_underscore = True
+            print(f"\n{the_gallows[chances_to_live]}\nYou lost 😞😢😭. The secret word was: ** {hidden_word} **")
+
+    if "_" not in word_in_char:
+        is_there_underscore = True
+        print(f"\nYou win🎉🍾!!! The secret word is: ** {hidden_word} **")
+
+
