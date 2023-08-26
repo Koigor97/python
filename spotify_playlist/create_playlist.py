7b3fdb9fae6e546a9a028d86dac29a9c74d99a2b
Koigor97 <turaysamuel101@yahoo.com>
2023-08-26 18:52:28 +0200

    Add create_playlist method

--- spotify_playlist/create_playlist.py
+++ spotify_playlist/create_playlist.py
@@ -13,11 +13,12 @@ class CreatePlaylist:
         self.__sp = ""
         self.__list_of_song = []
         self.__user_input = ""
+        self.__song_uris = []
         self.__get_test()
 
 
     def __get_test(self):
-        sp = spotipy.Spotify(
+        self.__sp = spotipy.Spotify(
             auth_manager=SpotifyOAuth(
                 scope="playlist-modify-private",
                 redirect_uri=self.__redirect_uri,
@@ -28,7 +29,7 @@ class CreatePlaylist:
                 username=self.__username
             )
         )
-        self.__user_id = sp.current_user()["id"]
+        self.__user_id = self.__sp.current_user()["id"]
 
 
     def __get_user_id(self):
@@ -37,8 +38,8 @@ class CreatePlaylist:
 
     def set_user_list(self, the_input, the_list):
         self.__user_input = the_input
-        self.__list_of_song.append(all(the_list))
-        print(self.__list_of_song)
+        self.__list_of_song += the_list
+
 
 
     def __get_user_input(self):
@@ -48,15 +49,25 @@ class CreatePlaylist:
     def __get_user_song_list(self):
         return self.__list_of_song
 
+
+    def __get_song_uris(self):
+        return self.__song_uris
+
+
     def search_for_songs(self):
-        song_uris = []
 
         year = self.__get_user_input().split("-")[0]
         for song in self.__get_user_song_list():
             result = self.__sp.search(q=f"track:{song} year:{year}", type="track")
-            print(result)
             try:
                 uri = result["tracks"]["items"][0]["uri"]
-                song_uris.append(uri)
+                self.__song_uris.append(uri)
             except IndexError:
                 print(f"{song} doesn't exist in Spotify. Skipped.")
+
+
+    def create_playlist(self):
+        playlist = self.__sp.user_playlist_create(user=self.__get_user_id(),
+                                                  name=f"{self.__get_user_input().split('-')[0]} Billboard 100",
+                                                  public=False)
+        self.__sp.playlist_add_items(playlist_id=playlist["id"], items=self.__get_song_uris())
