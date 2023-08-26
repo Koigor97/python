9087e355599276cda4dd366d2797582f09f2aa1c
Koigor97 <turaysamuel101@yahoo.com>
2023-08-26 17:51:22 +0200

    testing functionality

--- spotify_playlist/main.py
+++ spotify_playlist/main.py
@@ -1,11 +1,16 @@
 from get_songs_data import ScrapData
+from create_playlist import CreatePlaylist
 
 
 scrap_data = ScrapData()
+create_playlist = CreatePlaylist()
+
 
 year_query = input("Which year would like to search for (YY-MM-DD): ")
 scrap_data.set_year_of_search(year_query)
 data = scrap_data.the_data_recieve()[3:-11]
+create_playlist.set_user_list(the_input=year_query, the_list=data)
+create_playlist.search_for_songs()
 # print(len(data))
 # for song in data:
 #     print(song)
@@ -13,3 +18,4 @@ data = scrap_data.the_data_recieve()[3:-11]
 # while not year_query.isalpha():
 #     print(year_query.isalpha())
 #     year_query = input("Which year would like to search for (YY-MM-DD): ")
+
